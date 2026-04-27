b8096e12f317cdcaea134e143f111deb29875c21
Keinauff Ildikó <swainer20000228@gmail.com>
2026-04-27 15:46:07 +0200

    fix: category dto null fixed

--- RestfulAPI_01/Category.cs
+++ RestfulAPI_01/Category.cs
@@ -3,7 +3,7 @@
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; } = "";
+        public string? Name { get; set; }
 
         public ICollection<TodoItem> TodoItems { get; set; } = 
             new List<TodoItem>();
